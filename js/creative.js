4cbbb8a4f098ff21e4959850dd079a5c5caae25f
unknown <josehdezjnez@gmail.com>
2021-08-26 14:46:22 -0500

    RIC MANAGEMENT

---
+++ js/creative.js
@@ -0,0 +1,70 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Magnific popup calls
+  $('#portfolio').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1000,
+});
+$('collapse.shpw').each(function() {
+  $(this).prev('.card-header').find('.fas').addClass('fa-minus-circle').removeClass('fa-plus-circle');
+});
+  $('.collapse').on('.show.bs.collapse', function() {
+    $(this).prev('.card-header').find('.fas').removeClass('fa-plus-circle').addClass('fa-minus-circle');
+  }).on('hide.bscollapse', function() {
+    $(this).prev('.card-header').find('.fas').removeClass('fa-minus-circle').addClass('fa-plus-circle');
+  })
+})(jQuery); // End of use strict
